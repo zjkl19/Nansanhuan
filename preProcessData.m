4c7c1acd470ced8a057deb466dae211596e6d8b6
迪南 林 <eamdfan@hotmail.com>
2024-08-07 21:04:25 +0800

    add preProcessData

--- preProcessData.m
+++ preProcessData.m
@@ -2,7 +2,7 @@
 import src.*;
 
 % 创建对象
-processor = ExcelProcessor('data/nsh202406.xlsx');
+processor = ExcelProcessor('data/nsh202407.xlsx');
 
 % 匹配测点
 processor = processor.matchColumns('5L-4', false); % 精确匹配
@@ -20,7 +20,7 @@ processor.saveToFile();
 processor = processor.matchColumns('5L-11', false); % 精确匹配
 
 % 替换值，支持多个条件，但要确保用户输入不矛盾
-processor = processor.replaceValues(-220, 'less');
+processor = processor.replaceValues(-50, 'less');
 
 % 保存文件
 processor.saveToFile();
